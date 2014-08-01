4bb9f8e1d7b95ef57f47b75d189521aa5acbc961
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-08-01 14:29:57 +0200

    Initial Commit

---
+++ POI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POI
+//
+//  Created by Nicolas Besnard on 01/08/2014.
+//  Copyright (c) 2014 Nicolas Besnard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
